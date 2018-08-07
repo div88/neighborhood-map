b529a8e126951837fd207d23ad79c7dca99c3268
Divya Iyengar <divyaiyengar@Divyas-MacBook-Pro.local>
2018-08-07 11:00:49 -0700

    Filter and list feature added

--- src/Map.js
+++ src/Map.js
@@ -6,7 +6,9 @@ class Map extends Component {
   
     state = {
       isOpen: false,
-      infoId: ''
+      infoId: '',
+      allLocations: this.props.locations,
+      locations: this.props.locations
     }
   
     handleToggleOpen = (id) => {
@@ -21,19 +23,35 @@ class Map extends Component {
       infoId: id
     });
   }
+
+  updateQuery = (query) => {
+    var newLocations;
+    if(query.length > 0){
+      
+      newLocations = this.state.locations.filter((location) => {return location.title.toLowerCase().includes(query.toLowerCase())})
+      this.setState({
+        locations: newLocations
+      })
+    } else {
+      this.setState({
+        locations: this.state.allLocations
+      })
+    }
+    
+  }
   
   render() {
-    var locations = this.props.locations;
-   
-    const GoogleMapExample = withGoogleMap(props => (
+   let locations = this.state.locations
+    const MyMapComponent = withGoogleMap(props => (
       <GoogleMap
         defaultCenter = {{ lat: 37.77493, lng: -122.419416 }}
-        defaultZoom = { 8 }>
+        defaultZoom = { 12 }>
         
         {locations.map(marker => (
           <Marker
             key={marker.title}
             position={{ lat: marker.lat, lng: marker.lng }}
+            
             onClick={() => this.handleToggleOpen(marker.title)}>
             {(this.state.isOpen && this.state.infoId === marker.title)&&
               <InfoWindow onCloseClick={() => this.handleToggleClose(marker.title)}>
@@ -42,17 +60,31 @@ class Map extends Component {
             }
           </Marker>
       ))}
-      
-    />
+
       </GoogleMap>
    ));
    return(
       <div>
-        <GoogleMapExample
-          containerElement={ <div style={{ height: `500px`, width: '100%' }} /> }
-          mapElement={ <div style={{ height: `100%` }} /> }
-        />
+        <div className="search">
+                <div className="search-books-results" style={{  }}>
+                <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={(event) => this.updateQuery(event.target.value)}/>
+                    <ul>
+                      {locations.map((location) => (
+                        <li style={{ listStyle: `none` }} key={location.title} className="location">
+                          {location.title}
+                        </li>
+                      ))}             
+                    </ul>
 
+                    {this.state.isError && (
+                        <p className="error-label">No results available for this search. Try a different search term.</p>
+                    )}
+                </div>
+            </div>
+        {/* <MyMapComponent 
+          containerElement={ <div style={{ height: `100vh`, width: '100%' }} /> }
+          mapElement={ <div style={{ height: `100%` }} /> }
+        />              */}
         <div id="map"></div>
       </div>
    );
