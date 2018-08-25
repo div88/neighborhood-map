7c774afcd6e1ceb8d9f8144c65032c7bf3609ce8
Divya Iyengar <divyaiyengar@divyas-mbp.attlocal.net>
2018-08-25 00:16:14 -0700

    Updated readme, locations typo and Map list functionality

--- README.md
+++ README.md
@@ -1,45 +1,36 @@
-This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+# Neighborhood Map
 
-Below you will find some information on how to perform common tasks.<br>
-You can find the most recent version of this guide [here](https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md).
+Application utilizes the Google Maps API. A list-view of location names is provided which displays all locations by default, and displays the filtered subset of locations when a filter is applied.Clicking a location on the list displays unique information about the location. 
+
+Map displays all location markers by default, and displays the filtered subset of location markers when a filter is applied.Clicking a marker displays unique information about a location somewhere on the page.
 
 ## Table of Contents
 
-- [Updating to New Releases](#updating-to-new-releases)
-- [Sending Feedback](#sending-feedback)
-- [Folder Structure](#folder-structure)
-- [Available Scripts](#available-scripts)
-  - [npm start](#npm-start)
-  - [npm test](#npm-test)
-  - [npm run build](#npm-run-build)
-  - [npm run eject](#npm-run-eject)
-- [Supported Browsers](#supported-browsers)
-- [Supported Language Features and Polyfills](#supported-language-features-and-polyfills)
-- [Syntax Highlighting in the Editor](#syntax-highlighting-in-the-editor)
+- [Application Set Up](#application-setup)
+- [Project Specifications](#project-specifications)
+- [Contribution](#contribution)
+
+### Application Set Up
+
+* Clone or download the project from [here](https://github.com/div88/neighborhood-map)
+* Type 'npm install' in terminal to install all the necessary packages.
+* To run locally, 'npm start'
+
+
+### Project Specifications
+
+### Home Page
+
+* Loading home page, loads Google map with default set of markers and a lit of locations and a text-input field.
+* Includes a text input field or dropdown menu that filters the map markers and list items to locations matching the text input or selection. 
+* A list-view of location names is provided which displays all locations by default, and displays the filtered subset of locations when a filter is applied.
+Clicking a location on the list displays unique information about the location.
+* Map displays all location markers by default, and displays the filtered subset of location markers when a filter is applied.
+
+Clicking a marker displays unique information about a location somewhere on the page (modal, separate div, inside an infoWindow).
 
 
 
-## Updating to New Releases
+### Contributions
 
-Create React App is divided into two packages:
-
-* `create-react-app` is a global command-line utility that you use to create new projects.
-* `react-scripts` is a development dependency in the generated projects (including this one).
-
-You almost never need to update `create-react-app` itself: it delegates all the setup to `react-scripts`.
-
-When you run `create-react-app`, it always creates the project with the latest version of `react-scripts` so you’ll get all the new features and improvements in newly created apps automatically.
-
-To update an existing project to a new version of `react-scripts`, [open the changelog](https://github.com/facebookincubator/create-react-app/blob/master/CHANGELOG.md), find the version you’re currently on (check `package.json` in this folder if you’re not sure), and apply the migration instructions for the newer versions.
-
-In most cases bumping the `react-scripts` version in `package.json` and running `npm install` in this folder should be enough, but it’s good to consult the [changelog](https://github.com/facebookincubator/create-react-app/blob/master/CHANGELOG.md) for potential breaking changes.
-
-We commit to keeping the breaking changes minimal so you can upgrade `react-scripts` painlessly.
-
-## Sending Feedback
-
-We are always open to [your feedback](https://github.com/facebookincubator/create-react-app/issues).
-
-Contributing
-
-https://google-developers.appspot.com/maps/documentation/utils/geocoder/#place_id%3DChIJ_T25cNd_j4ARehGmHe0pT84
+React google maps (https://tomchentw.github.io/react-google-maps/#overlayview)
--- src/Map.js
+++ src/Map.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
 
 class Map extends Component {
-  
-  
     state = {
       isOpen: false,
       infoId: '',
@@ -11,18 +9,24 @@ class Map extends Component {
       locations: this.props.locations
     }
   
-    handleToggleOpen = (id) => {
+    handleToggleOpen = (item) => {
       this.setState({
         isOpen: true,
-        infoId: id
+        infoId: item.title,
+        position: {
+          lat: item.lat,
+          lng: item.lng
+        }
       });
     }
-  handleToggleClose = (id) => {
-    this.setState({
-      isOpen: false,
-      infoId: id
-    });
-  }
+
+    handleToggleClose = (id) => {
+      this.setState({
+        isOpen: false,
+        infoId: id,
+      
+      });
+    }
 
   updateQuery = (query) => {
     var newLocations;
@@ -39,55 +43,59 @@ class Map extends Component {
     }
     
   }
+
+  // Clicking a location on the list displays unique information about the location, and animates its associated map marker (e.g. bouncing, color change.)
   
   render() {
    let locations = this.state.locations
     const MyMapComponent = withGoogleMap(props => (
       <GoogleMap
-        defaultCenter = {{ lat: 37.77493, lng: -122.419416 }}
-        defaultZoom = { 12 }>
-        
-        {locations.map(marker => (
-          <Marker
-            key={marker.title}
-            position={{ lat: marker.lat, lng: marker.lng }}
-            
-            onClick={() => this.handleToggleOpen(marker.title)}>
-            {(this.state.isOpen && this.state.infoId === marker.title)&&
-              <InfoWindow onCloseClick={() => this.handleToggleClose(marker.title)}>
+          defaultCenter = {{ lat: 37.77493, lng: -122.419416 }}
+          defaultZoom = { 12 }>
+          
+          this.marker = {locations.map(marker => (
+            <Marker
+              key={marker.title}
+              position={{ lat: marker.lat, lng: marker.lng }}
+              
+            onClick={() => this.handleToggleOpen(marker)}>
+            {(this.state.position && this.state.infoId === marker.title) &&
+              <InfoWindow  position={this.state.position}>
                 <span>{marker.title}</span>
               </InfoWindow>
-            }
-          </Marker>
-      ))}
-
+              }
+            </Marker>
+        ))}
       </GoogleMap>
-   ));
-   return(
+    ));
+    return(
       <div>
         <div className="search">
-                <div className="search-books-results" style={{  }}>
-                <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={(event) => this.updateQuery(event.target.value)}/>
-                    <ul>
-                      {locations.map((location) => (
-                        <li style={{ listStyle: `none` }} key={location.title} className="location">
-                          {location.title}
-                        </li>
-                      ))}             
-                    </ul>
+          <div className="search-books-results" style={{  }}>
+          <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={(event) => this.updateQuery(event.target.value)}/>
+              <ul>
+                {locations.map((location) => (
+                  <li style={{ listStyle: `none` }} key={location.title} className="location" onClick={() => this.handleToggleOpen(location)}>
+                  {(this.state.position && this.state.infoId.title === location.title) &&
+                    <p>{location.title}</p>
+                  }
+                  {location.title}
+                </li>
+                ))}             
+              </ul>
 
-                    {this.state.isError && (
-                        <p className="error-label">No results available for this search. Try a different search term.</p>
-                    )}
-                </div>
-            </div>
-        {/* <MyMapComponent 
+              {this.state.isError && (
+                  <p className="error-label">No results available for this search. Try a different search term.</p>
+              )}
+          </div>
+        </div>
+        <MyMapComponent 
           containerElement={ <div style={{ height: `100vh`, width: '100%' }} /> }
           mapElement={ <div style={{ height: `100%` }} /> }
-        />              */}
+        />             
         <div id="map"></div>
       </div>
-   );
+    );
    }
 };
 export default Map;
